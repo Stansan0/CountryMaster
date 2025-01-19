047ae12c4dd8289a4c71ba4b5f8035e58debbfba
DESKTOP-VVKMCUE\HP <stevensanchezhuacon@gmail.com>
2025-01-19 15:02:03 -0500

    Intent button principal 2

---
+++ app/src/main/java/com/example/countrymaster/FondoMundo1.kt
@@ -0,0 +1,16 @@
+package com.example.countrymaster
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FondoMundo1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fondo_mundo1)
+
+    }
+}
