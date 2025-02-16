a2e3fb557900508426e85dca8bd9bfd628f8d945
DESKTOP-VVKMCUE\HP <stevensanchezhuacon@gmail.com>
2025-02-16 17:44:45 -0500

    1

--- app/src/main/java/com/example/countrymaster/login1.kt
+++ app/src/main/java/com/example/countrymaster/login1.kt
@@ -1,25 +1,32 @@
 package com.example.countrymaster
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class login1 : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login1)
-        val buttonPrincipal = findViewById<Button>(R.id.buttonPrincipal)
-        Toast.makeText(this,"onCreate", Toast.LENGTH_SHORT).show();
-        buttonPrincipal.setOnClickListener {
-            startActivity(Intent(this,FondoMundo1::class.java))
+
+        // Configuración para aplicar los márgenes según el sistema de barras
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Configurar el botón 'Iniciar sesión' con la acción de Intent
+        val iniciarSesionButton = findViewById<Button>(R.id.button)
+        iniciarSesionButton.setOnClickListener {
+            // Iniciar la actividad FondoMundo1
+            val intent = Intent(this, FondoMundo1::class.java)
+            startActivity(intent)
         }
     }
 }
